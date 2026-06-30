caca4e60bdc73985f274493cee5032ec437578f9
seoyoungyouk <seoyoungyouk@gmail.com>
2026-06-30 10:23:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showMessage() {
   const message = document.getElementById("message");
-  message.textContent = "Thanks for visiting my portfolio!";
+  message.textContent = "Email: your.email@example.com";
 }
